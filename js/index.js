f1ace7f31020328ddc0ea61ff46e08193d8682ba
vladysanys <111743049+vladysanys@users.noreply.github.com>
2022-11-15 16:17:22 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,53 @@
-// const input = document.querySelector('.input')
-// const znakC = document.querySelector('.c')
-// const procents = document.querySelector('.procetn')
-// const procent = document.querySelector('.procent')
-// const delenie = document.querySelector('.delenie')
-// const num7 = document.querySelector('')
-const buttons = document.querySelector('button')
-const buttonText = document.querySelector('.buttons-text')
-console.log(buttonText.textContent)
-buttons.addEventListener('click', () => {
-    let num = buttons.closest('p')
-    console.log(num.)
+const input = document.querySelector('.input')
+const znakC = document.querySelector('.c')
+const nums = document.querySelectorAll('.nums')
+const minus = document.querySelector('.minus')
+const plus = document.querySelector('.plus')
+const equals = document.querySelector('.equal')
+const plusMinus = document.querySelector('.plus-minus')
+let arr = []
+let sum = ''
+document.addEventListener('click', (event) => {
+    const {target} = event
+    let num = 0
+    let num1 = arr.length - 1
+    let num2 = arr.length
+    if(znakC.id === target.id) {
+        input.value = ''
+        sum = 'c'
+        arr = []
+    }
+    nums.forEach(function(item,i) {
+        if(nums[i].id === target.id) {
+            input.value = input.value + target.outerText
+        }
+    })
+    if(minus.id === target.id) {
+        num = Number(input.value)
+        arr.push(num)
+        input.value = ''
+        sum = 'minus'
+    } else if (plus.id === target.id) {
+        num = Number(input.value)
+        arr.push(num)
+        input.value = ''
+        sum = 'plus'
+    } else if (plusMinus.id === target.id) {
+        num = Number(input.value)
+        arr.push(num)
+        input.value = arr[num2] * -1
+        sum = 'plusMinus'
+    }
+    if(equals.id === target.id) {
+        num = Number(input.value)
+        arr.push(num)
+        if (sum === 'minus') {
+            input.value = arr[num1] - arr[num2]
+        } else if (sum === 'plus') {
+            input.value = arr[num1] + arr[num2]
+        }
+    }
+    console.log(sum)
+    console.log(arr)
+    console.log(arr.length - 1)
 })
